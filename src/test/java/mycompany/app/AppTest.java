0e76d65123ee82bccc5b1a651775a1162ff44ea3
khairusyaidy <khaimalevolency@gmail.com>
2024-07-22 00:51:28 +0800

    test

--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest
 {
 	WebDriver driver; 
 	WebDriverWait wait; 
-	String url = "http://172.18.0.3:8081";
+	String url = "http://127.0.0.1:8081";
 	String validEmail = "user@example.com";
 	String validPassword = "password1234";
 	String invalidEmail = "none@example.com";
