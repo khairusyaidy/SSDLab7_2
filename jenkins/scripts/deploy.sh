1bf7c164bbe814886a050d01952220b622029b62
khairusyaidy <khaimalevolency@gmail.com>
2024-07-22 00:56:35 +0800

    test

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -3,6 +3,7 @@
 set -x
 docker run -d -p 8081:80 --name my-apache-php-app -v /c/Users/khair/Documents/Lab/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
 sleep 1
+docker ps
 set +x
 
 echo 'Now...'
