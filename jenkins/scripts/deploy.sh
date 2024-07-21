c84e6e05739397649cc471a48ecd514414a90414
khairusyaidy <khaimalevolency@gmail.com>
2024-07-22 02:16:34 +0800

    test

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 8082:80 --name my-apache-php-app -v /var/jenkins_home/workspace/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
+docker network create jenkins
+docker run -d --network jenkins -p 8082:80 --name my-apache-php-app -v /var/jenkins_home/workspace/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
 sleep 1
 docker ps
-docker network create jenkins
-docker run -d --network jenkins --name my-php-app -p 8082:80 php:7.2-apache
+
 
 set +x
 
