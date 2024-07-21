5b0f66cba7fa5565d72259d9354458c3da5eb776
khairusyaidy <khaimalevolency@gmail.com>
2024-07-22 01:02:01 +0800

    test

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -4,6 +4,7 @@ set -x
 docker run -d -p 8081:80 --name my-apache-php-app -v /c/Users/khair/Documents/Lab/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
 sleep 1
 docker ps
+docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' my-apache-php-app
 set +x
 
 echo 'Now...'
